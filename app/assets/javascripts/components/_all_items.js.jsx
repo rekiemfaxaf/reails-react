4c20accb32cd5c963f221c258e00ab5e255e272e
Felipe Valdivia Vivar <pipe.faxaf@gmail.com>
2017-08-22 23:08:41 +0000

    new item by ajax request

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,16 +1,6 @@
 var AllItems = React.createClass({ 
-    getInitialState() { 
-        return { items: [] } 
-        
-    },
-    
-    componentDidMount() { 
-        console.log('Component mounted'); 
-        $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
-    }, 
-    
     render() { 
-        var items= this.state.items.map((item) => { 
+        var items= this.props.items.map((item) => { 
             return ( 
                 <div key={item.id}>
                     <h3>{item.name}</h3> 
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,8 +1,25 @@
 var Body  = React.createClass({ 
+    getInitialState() { 
+        return { items: [] } 
+        
+    },
+    
+    componentDidMount() { 
+        console.log('Component mounted'); 
+        $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
+    }, 
+    
+    handleSubmit(item) {
+        var newState = this.state.items.concat(item); 
+        this.setState({ items: newState })
+        console.log(item);
+    },
+    
     render() { 
         return ( 
             <div> 
-                <AllItems />
+                <NewItem handleSubmit={this.handleSubmit}/>
+                <AllItems items={this.state.items}/>
             </div> 
         )  
     }
